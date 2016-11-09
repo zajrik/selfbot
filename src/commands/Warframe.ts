590254340cbb4edfd3edac21cc510f4f0a63ae04
Zack Campbell <zajrik@gmail.com>
2016-11-09 17:52:29 -0600

    Update Warframe bot command to support edited output
    Some of the commands in the rewritten warframe bot now send a message to let the caller know it's processing before editing the message with the final result. Didn't think about how that would change this command for my selfbot.

--- src/commands/Warframe.ts
+++ src/commands/Warframe.ts
@@ -1,6 +1,6 @@
 'use strict';
 import { Bot, Command } from 'yamdbf';
-import { User, Message, Collection } from 'discord.js';
+import { User, Message, Collection, MessageCollector } from 'discord.js';
 
 export default class Warframe extends Command
 {
@@ -18,17 +18,43 @@ export default class Warframe extends Command
 
 	public async action(message: Message, args: Array<string | number>, mentions: User[], original: string): Promise<any>
 	{
-		message.edit(`_Waiting for Warframe bot response..._`);
-		const wfBot: User = this.bot.users.get('219977426036457483');
+		message = await message.edit(`_Waiting for Warframe bot response..._`);
+
 		const prune: number = <number> (!isNaN(<number> args[0]) ? args[0] : null);
 		const command: string = <string> (prune ? args[1] : args[0]);
 		const cmdArgs: string = <string> (prune ? args.slice(2) : args.slice(1)).join(' ') || null;
+
+		const wfBot: User = this.bot.users.get('219977426036457483');
 		const cmdMsg: Message = <Message> await wfBot.sendMessage(`${command} ${cmdArgs ? cmdArgs : ''}`);
-		const response: Collection<string, Message> = await cmdMsg.channel
-			.awaitMessages(a => a.author.id === wfBot.id, { max: 1, time: 10000 });
-		if (!response.first()) return message.edit('Warframe bot response timed out.')
-			.then((res: Message) => res.delete(5000));
-		return message.edit(response.first().content).then(res =>
-			prune ? (<Message> res).delete(prune * 1000) : null);
+
+		const collector: MessageCollector = cmdMsg.channel.createCollector(
+			a => a.author.id === wfBot.id, { max: 1, time: 10000 });
+
+		let output: Message;
+		let pruneTimer: NodeJS.Timer;
+
+		collector.on('message', async (msg: Message) =>
+		{
+			output = <Message> await message.edit(msg.content);
+			pruneTimer = this.bot.setTimeout(() => { if (prune) output.delete(); }, prune * 1000);
+			const update: Function = (old: Message, changed: Message) =>
+			{
+				if (changed.author.id !== wfBot.id
+					&& changed.channel.id !== cmdMsg.channel.id) return;
+				output.edit(changed.content)
+					.then((res: Message) => prune ? res.delete(prune * 1000) : null);
+				this.bot.clearTimeout(pruneTimer);
+				this.bot.removeListener('messageUpdate', update);
+			};
+			this.bot.on('messageUpdate', update);
+			this.bot.setTimeout(() => this.bot.removeListener('messageUpdate', update), 10000);
+		})
+		.on('end', (collected: Collection<string, Message>, reason: string) =>
+		{
+			if (reason !== 'time') return;
+			this.bot.clearTimeout(pruneTimer);
+			output.edit('WarframeBot response timed out.')
+				.then((res: Message) => res.delete(5000));
+		});
 	}
 };
