a6250cc58a93d41eb4340b11c954eef7687dc08a
Zack Campbell <zajrik@gmail.com>
2016-12-13 11:16:04 -0600

    Use embed for stats command output

--- src/commands/Stats.ts
+++ src/commands/Stats.ts
@@ -1,7 +1,7 @@
 'use strict';
 import { Bot, Command, version } from 'yamdbf';
 import * as Discord from 'discord.js';
-import { User, Message } from 'discord.js';
+import { User, Message, RichEmbed } from 'discord.js';
 import Time from '../lib/Time';
 
 export default class Stats extends Command
@@ -20,15 +20,19 @@ export default class Stats extends Command
 
 	public action(message: Message, args: Array<string | number>, mentions: User[], original: string): any
 	{
-		message.edit(`\`\`\`css\n`
-			+ `SELFBOT STATISTICS\n`
-			+ `• Mem Usage  : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB\n`
-			+ `• Uptime     : ${Time.difference(this.bot.uptime * 2, this.bot.uptime).toString()}\n`
-			+ `• Users      : ${this.bot.users.size}\n`
-			+ `• Servers    : ${this.bot.guilds.size}\n`
-			+ `• Channels   : ${this.bot.channels.size}\n`
-			+ `• YAMDBF     : v${version}\n`
-			+ `• Discord.js : v${Discord.version}\n`
-			+ `\`\`\``);
+		const embed: RichEmbed = new RichEmbed()
+			.setAuthor('Selfbot Statistics', this.bot.user.avatarURL)
+			.setColor(8450847)
+			.addField('Mem Usage', `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true)
+			.addField('Uptime', Time.difference(this.bot.uptime * 2, this.bot.uptime).toString(), true)
+			.addField('\u200b', '\u200b', true)
+			.addField('Users', this.bot.users.size.toString(), true)
+			.addField('Servers', this.bot.guilds.size.toString(), true)
+			.addField('Channels', this.bot.channels.size.toString(), true)
+			.addField('YAMDBF', `v${version}`, true)
+			.addField('Discord.js', `v${Discord.version}`, true)
+			.addField('\u200b', '\u200b', true);
+
+		message.channel.sendEmbed(embed);
 	}
 }
