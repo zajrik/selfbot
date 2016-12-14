163e80dea5908dfa25e033c95b3cc8e662cb4f68
Zack Campbell <zajrik@gmail.com>
2016-12-14 13:39:35 -0600

    Update stats command

--- src/commands/Stats.ts
+++ src/commands/Stats.ts
@@ -27,13 +27,16 @@ export default class Stats extends Command
 			.addField('Mem Usage', `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true)
 			.addField('Uptime', Time.difference(this.bot.uptime * 2, this.bot.uptime).toString(), true)
 			.addField('\u200b', '\u200b', true)
-			.addField('Users', this.bot.guilds.map(g => g.memberCount).reduce((a, b) => a + b), true)
 			.addField('Servers', this.bot.guilds.size.toString(), true)
 			.addField('Channels', this.bot.channels.size.toString(), true)
+			.addField('Users', this.bot.guilds.map(g => g.memberCount).reduce((a, b) => a + b), true)
 			.addField('YAMDBF', `v${version}`, true)
 			.addField('Discord.js', `v${Discord.version}`, true)
-			.addField('\u200b', '\n\u200b', true)
+			.addField('\u200b', '\u200b', true)
+			.addField('Selfbot Source', '[Available on GitHub](https://github.com/zajrik/selfbot)', true)
+			.addField('YAMDBF Info', 'https://yamdbf.js.org', true)
 			.setFooter('YAMDBF', this.bot.user.avatarURL)
+			.addField('\u200b', '\u200b', true)
 			.setTimestamp();
 
 		message.channel.sendEmbed(embed);
